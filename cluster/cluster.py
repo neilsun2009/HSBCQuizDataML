f9943b0aeb4487bb51cd574123fddaf263872757
neilsun2009 <neilsun2--0@163.com>
2023-07-07 14:11:01 +0000

    [add][keyword]

--- cluster/cluster.py
+++ cluster/cluster.py
@@ -24,7 +24,7 @@ def main():
     parser.add_argument('--batch_size', type=int, default=1024, help='batch size for cluster methods')
     parser.add_argument('--aug_customer_csv_path', type=str, required=True, help='output csv file for augmented info on customers')
     parser.add_argument('--output_model_dir', type=str, required=True, help='output dir for kmeans & pca models')
-    parser.add_argument('--max_data_lines', type=int, default=-1, help='max amount of data lines to be handled')
+    parser.add_argument('--max_data_lines', type=int, default=-1, help='max amount of data lines to calculate kmeans & pca')
     args = parser.parse_args()
 
     output_kmeans_path = os.path.join(args.output_model_dir, 'kmeans.pkl')
@@ -57,14 +57,39 @@ def main():
     labels = kmeans.fit_predict(vector_data)
     print(f'Inertia: {kmeans.inertia_}')
 
+    if args.max_data_lines != -1:
+        print('Loading rest of vector data and predict...')
+        rest_vector_data = []
+        line_number = 0
+        with open(args.vector_path, 'rb') as f:
+            while True:
+                try:
+                    line_number += 1
+                    if line_number > args.max_data_lines:
+                        rest_vector_data.append(np.load(f))
+                    # if line_number == 2000:
+                    #     break
+                    else:
+                        temp = np.load(f)
+                    if line_number % 100000 == 0:
+                        print(f'read {line_number} lines')
+                except:
+                    break
+        pca_data = np.concatenate((pca_data, pca.transform(rest_vector_data)), axis=0)
+        labels = np.concatenate((labels, kmeans.predict(rest_vector_data)), axis=0)
+        vector_data += rest_vector_data
+        del rest_vector_data
+
     print('Calculating distances...')
     customer_ids = []
     distances = []
     with open(args.customer_id_path) as f:
         with tqdm(total=len(labels)) as pbar:
             for idx, line in enumerate(f):
-                if args.max_data_lines != -1 and idx >= args.max_data_lines:
-                    break
+                # if args.max_data_lines != -1 and idx >= args.max_data_lines:
+                #     break
+                # if idx >= 2000:
+                #     break
                 customer_id = line.strip()
                 customer_ids.append(customer_id)
                 distances.append(np.linalg.norm(vector_data[idx] - kmeans.cluster_centers_[labels[idx]]))
@@ -81,7 +106,8 @@ def main():
     aug_df['vector2dY'] = pca_data[:, 1]
 
     print('Merging comment file...')
-    customer_df = pd.read_csv(args.customer_csv_path, nrows=None if args.max_data_lines == -1 else args.max_data_lines)
+    # customer_df = pd.read_csv(args.customer_csv_path, nrows=None if args.max_data_lines == -1 else args.max_data_lines)
+    customer_df = pd.read_csv(args.customer_csv_path)
     
     customer_df = pd.merge(customer_df, aug_df, how='left', 
                             left_on="customerId", right_on="customerId")
